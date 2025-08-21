d6964671964099be1ba01918709b6f1d321408e1
Galo <durantegalo@gmail.com>
2025-08-21 12:27:30 -0300

    Install shadcn/ui and config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
+import { redirect } from 'next/navigation';
+
 export default function Home() {
-    return (
-        <div className="p-4">
-            <h1 className="text-2xl font-bold">Home page</h1>
-        </div>
-    );
+    redirect('/talentos');
 }
---
+++ src/app/talentos/page.tsx
@@ -0,0 +1,7 @@
+export default function TalentsPage() {
+    return (
+        <div>
+            <h1>Talents page</h1>
+        </div>
+    );
+}
