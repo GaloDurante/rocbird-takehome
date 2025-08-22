b0127d3c81c0e809d81af35d7687396802e5a544
Galo <durantegalo@gmail.com>
2025-08-22 00:45:23 -0300

    Implement Dialog and Toast from shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import '@/app/globals.css';
 
+import { Toaster } from '@/components/ui/sonner';
+
 const geistSans = Geist({
     variable: '--font-geist-sans',
     subsets: ['latin'],
@@ -30,6 +32,7 @@ export default function RootLayout({
         <html lang="en" className="dark">
             <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
                 <main>{children}</main>
+                <Toaster richColors position="top-right" />
             </body>
         </html>
     );
